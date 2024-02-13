706ef99e8c234fd7776a057212c72094a8968fbc
brain <vaibhav.satam@accionlabs.com>
2024-02-13 13:11:07 +0530

    created crud for patient

--- src/main/java/com/accion/consultation/mappers/PatientMapper.java
+++ src/main/java/com/accion/consultation/mappers/PatientMapper.java
@@ -4,6 +4,7 @@ import com.accion.consultation.entities.PatientEntity;
 import com.accion.consultation.entities.PersonName;
 import com.accion.consultation.models.PatientStatus;
 import com.accion.consultation.models.YesOrNoIndicator;
+import com.accion.consultation.models.dto.NameDTO;
 import com.accion.consultation.models.dto.patient.CreatePatientRequestDTO;
 import com.accion.consultation.models.dto.patient.PatientDTO;
 import com.accion.consultation.models.dto.patient.UpdatePatientRequestDTO;
@@ -13,19 +14,89 @@ import org.springframework.stereotype.Component;
 public class PatientMapper implements EntityMapper<PatientEntity, PatientDTO> {
     @Override
     public PatientEntity toEntity(PatientDTO model) {
-        return null;
+        PersonName personName = new PersonName();
+        personName.setGivenName(model.getName().getGivenName());
+        personName.setMiddleName(model.getName().getMiddleName());
+        personName.setFamilyName(model.getName().getFamilyName());
+
+        PatientEntity patient = new PatientEntity();
+        patient.setUsername(model.getEmail());
+        patient.setName(personName);
+        patient.setEmail(model.getEmail());
+        patient.setDob(model.getDob());
+        patient.setMrn(model.getMrn());
+        patient.setSsn(model.getSsn());
+        patient.setLicenseNumber(model.getLicenseNumber());
+        patient.setMaritalStatus(model.getMaritalStatus());
+        patient.setAdministrativeSex(model.getAdministrativeSex());
+        patient.setIs_deceased(YesOrNoIndicator.N);
+        patient.setStatus(PatientStatus.ACTIVE);
+
+        return patient;
     }
 
     @Override
     public PatientDTO toModel(PatientEntity entity) {
-        return PatientDTO.builder().build();
+        NameDTO personName = new NameDTO();
+        personName.setGivenName(entity.getName().getGivenName());
+        personName.setMiddleName(entity.getName().getMiddleName());
+        personName.setFamilyName(entity.getName().getFamilyName());
+
+        PatientDTO patient = new PatientDTO();
+        patient.setPatientId(entity.getUserId());
+        patient.setName(personName);
+        patient.setEmail(entity.getEmail());
+        patient.setDob(entity.getDob());
+        patient.setMrn(entity.getMrn());
+        patient.setSsn(entity.getSsn());
+        patient.setLicenseNumber(entity.getLicenseNumber());
+        patient.setMaritalStatus(entity.getMaritalStatus());
+        patient.setAdministrativeSex(entity.getAdministrativeSex());
+        patient.setIs_deceased(entity.getIs_deceased());
+        patient.setDeceased_on(entity.getDeceased_on());
+        patient.setStatus(entity.getStatus());
+        patient.setCreatedAt(entity.getCreatedAt());
+        patient.setUpdatedAt(entity.getUpdatedAt());
+        return patient;
     }
 
     public PatientEntity toCreatePatientEntity(CreatePatientRequestDTO model) {
-        return null;
+        PersonName personName = new PersonName();
+        personName.setGivenName(model.getName().getGivenName());
+        personName.setMiddleName(model.getName().getMiddleName());
+        personName.setFamilyName(model.getName().getFamilyName());
+
+        PatientEntity patient = new PatientEntity();
+        patient.setUsername(model.getEmail());
+        patient.setName(personName);
+        patient.setEmail(model.getEmail());
+        patient.setDob(model.getDob());
+        patient.setMrn(model.getMrn());
+        patient.setSsn(model.getSsn());
+        patient.setLicenseNumber(model.getLicenseNumber());
+        patient.setMaritalStatus(model.getMaritalStatus());
+        patient.setAdministrativeSex(model.getAdministrativeSex());
+        patient.setIs_deceased(YesOrNoIndicator.N);
+        patient.setStatus(PatientStatus.ACTIVE);
+        return patient;
     }
 
     public PatientEntity toUpdatePatientEntity(UpdatePatientRequestDTO model) {
-        return null;
+        PersonName personName = new PersonName();
+        personName.setGivenName(model.getName().getGivenName());
+        personName.setMiddleName(model.getName().getMiddleName());
+        personName.setFamilyName(model.getName().getFamilyName());
+
+        PatientEntity patient = new PatientEntity();
+        patient.setName(personName);
+        patient.setEmail(model.getEmail());
+        patient.setDob(model.getDob());
+        patient.setMrn(model.getMrn());
+        patient.setSsn(model.getSsn());
+        patient.setLicenseNumber(model.getLicenseNumber());
+        patient.setMaritalStatus(model.getMaritalStatus());
+        patient.setAdministrativeSex(model.getSex());
+
+        return patient;
     }
 }
--- src/main/java/com/accion/consultation/models/dto/patient/PatientDTO.java
+++ src/main/java/com/accion/consultation/models/dto/patient/PatientDTO.java
@@ -6,25 +6,22 @@ import com.accion.consultation.models.PatientStatus;
 import com.accion.consultation.models.YesOrNoIndicator;
 import com.accion.consultation.models.dto.NameDTO;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
-import java.time.LocalDate;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class PatientDTO {
-    private long id;
+    private long patientId;
     private String mrn;
     private String ssn;
     private NameDTO name;
     private String email;
     private Instant dob;
-    private AdministrativeSex sex;
+    private AdministrativeSex administrativeSex;
     private String licenseNumber;
     private MaritalStatus maritalStatus;
     private YesOrNoIndicator is_deceased;
