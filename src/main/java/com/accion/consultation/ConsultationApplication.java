19a8093964ad23e684f51aa110182ad32935cbc2
Vaibhav Satam <vaibhav.satam@accionlabs.com>
2024-05-24 10:58:20 +0530

    merged development

--- src/main/java/com/accion/consultation/ConsultationApplication.java
+++ src/main/java/com/accion/consultation/ConsultationApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsultationApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ConsultationApplication.class, args);
-	}
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(ConsultationApplication.class, args);
+    }
 }
